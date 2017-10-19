7ea8ceb387f709ae392242568e27a64f35923118
gffuma <fumagalli.gf@gmail.com>
2017-10-19 10:35:49 +0200

    Pretty output tests

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const program = require('commander')
 const _ = require('lodash')
 const moment = require('moment')
 const colors = require('colors')
+const emoji = require('node-emoji')
 
 const DB_FILE = path.resolve(__dirname, './data/sbatte.json')
 const T = 'YYYY-MM-DD HH:mm'
@@ -18,6 +19,28 @@ const sessionsMinutesSpent = sessions => sessions.reduce((minutes, s) => {
   const stop = moment(s.stop, T)
   return minutes + stop.diff(start, 'minutes')
 }, 0)
+
+const log = (msg = '') => process.stdout.write(`${msg}\n`)
+
+// console.prototype.log = function() {
+//   this._stdout.write(util.format.apply(this, arguments) + '\n');
+// };
+//
+// console.log(emoji.get('coffe') + '\n')
+log()
+log(`${emoji.get('monkey')}    swapick-bug-drago  ${emoji.get('hourglass_flowing_sand')}  140 Min = 2 H 40 Min  ~  (8.00 - <sbatta/>)`.yellow)
+log('     Fare storie matte ahahah'.yellow)
+log('     #swapick'.yellow)
+log()
+log(`${emoji.get('zzz')}    ww1`)
+log(`${emoji.get('zzz')}    swapick-bug-drago  ${emoji.get('hourglass_flowing_sand')}  140 Min = 2 H 40 Min ~   (8.00 - <sbatta/>) `)
+log()
+log(`${emoji.get('rocket')}    swapick-bug-drago  ${emoji.get('hourglass_flowing_sand')}  140 Min = 2 H 40 Min ~  (8.00 - 9.00) (10.00 - 11.32) http://workplan.inmagik.com/projects/3/activities/274`.green)
+log(`${emoji.get('rocket')}    swapick-bug-drago  ${emoji.get('hourglass_flowing_sand')}  140 Min = 2 H 40 Min ~  (8.00 - 9.00) (10.00 - 11.32)`.green)
+log()
+// process.stdout.write(`${emoji.get('coffe')}\n`)
+// process.stdout.write(emoji.get('rocket')+'\n');
+
 
 program
   .version('0.1.0')
@@ -35,17 +58,18 @@ program
     const sbatte = readSbatte()
     const sessions = _.get(sbatte, name, [])
 
-    if (sessions.length > 0 && !sessions[sessions.length - 1].stop) {
-      console.log('Sbatta alredy started, you must first close it! Sooocio'.red)
-      return
-    }
+    // if (sessions.length > 0 && !sessions[sessions.length - 1].stop) {
+    //   console.log('Sbatta alredy started, you must first close it! Sooocio'.red)
+    //   return
+    // }
+    //
+    // writeSbatte({
+    //   ...sbatte,
+    //   [name]: sessions.concat({ start: moment().format(T) })
+    // })
 
-    writeSbatte({
-      ...sbatte,
-      [name]: sessions.concat({ start: moment().format(T) })
-    })
-
-    console.log('Sbatta started'.green)
+    console.log('\u1F37A')
+    // console.log('Sbatta started', emoji.get('coffe'))
   })
 
 program
